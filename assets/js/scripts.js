2b40895358b8eaae9e39c70fb81abedbc1ea9260
annia valdes <millerl.mb79@gmail.com>
2020-06-27 12:13:22 -0500

    update function searchForecast for display the data for 5 days

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,8 +2,16 @@ var apiKey = "f859703a85a79facc803ec19e50f064c";
 var now = moment().format("(L)"); //object moment
 var weatherDay = document.querySelector("#showCurrentWeather")
 var weatherInfo = document.querySelector("#weather-container")
-var daysFore=document.querySelector("#forecast")
-var  divAllDay =document.querySelector("#allday")
+//var daysFore = document.querySelector("#forecast")
+//var divAllDay = document.querySelector("#allday")
+var title= document.querySelector("#tittle5")
+//forecast
+var day1 = document.querySelector("#eachday1")
+//var divTwo = document.querySelector("#two")
+var day2 = document.querySelector("#eachday2")
+var day3 = document.querySelector("#eachday3")
+var day4 = document.querySelector("#eachday4")
+var day5 = document.querySelector("#eachday5")
 
 //var total=document.querySelector("#totalInfo")
 //elements for show wether data
@@ -19,91 +27,91 @@ var UVS = document.createElement("p");
 // llamas a show the city history
 
 function searchCurrent(city) {
-    
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
-        .then(function (response) {
-            return response.json()
-           
-        }).then(function (data) {
-           // console.log(data)
 
-             //show city name and current day
-             var dateCity = document.createElement("p"); //add the icon
-             dateCity.innerHTML = city + " " + now;
-             weatherDay.innerHTML="";
-             weatherDay.classList="currentStyle"
-             weatherDay.appendChild(dateCity);
-            
-            //get info from API
-            var temperature = data.main.temp;
-            var humidity = data.main.humidity;
-            var windSpeed = data.wind.speed;
-            var coordlat = data.coord.lat;
-            var coordlon = data.coord.lon;
-            
-           //clear the info
-            weatherInfo.innerHTML="";
-            listdata.innerHTML="";
+  fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`)
+    .then(function (response) {
+      return response.json()
 
-             //call UV for get that info passing coords
-            searchUV(coordlat, coordlon);
+    }).then(function (data) {
+      // console.log(data)
 
-            //display on page the weather data
-           
-            tempS.textContent = " Temperature:" + " " + temperature + " " + "ºF";
-            humS.textContent = " Humidity:" + " " + humidity + " " + "%";
-            windS.textContent = " Wind" + " " + "Speed:" + " " + windSpeed + " " + "MPH";
+      //show city name and current day
+      var dateCity = document.createElement("p"); //add the icon
+      dateCity.innerHTML = city + " " + now;
+      weatherDay.innerHTML = "";
+      weatherDay.classList = "currentStyle"
+      weatherDay.appendChild(dateCity);
 
-            weatherInfo.classList="card"
+      //get info from API
+      var temperature = data.main.temp;
+      var humidity = data.main.humidity;
+      var windSpeed = data.wind.speed;
+      var coordlat = data.coord.lat;
+      var coordlon = data.coord.lon;
 
-            listdata.appendChild( weatherDay)
-            listdata.appendChild(tempS)
-            listdata.appendChild(humS)
-            listdata.appendChild(windS)           
-            weatherInfo.appendChild(listdata)
-           
-        })
-       
+      //clear the info
+      weatherInfo.innerHTML = "";
+      listdata.innerHTML = "";
+
+      //call UV for get that info passing coords
+      searchUV(coordlat, coordlon);
+
+      //display on page the weather data
+
+      tempS.textContent = " Temperature:" + " " + temperature + " " + "ºF";
+      humS.textContent = " Humidity:" + " " + humidity + " " + "%";
+      windS.textContent = " Wind" + " " + "Speed:" + " " + windSpeed + " " + "MPH";
+
+      weatherInfo.classList = "card"
+
+      listdata.appendChild(weatherDay)
+      listdata.appendChild(tempS)
+      listdata.appendChild(humS)
+      listdata.appendChild(windS)
+      weatherInfo.appendChild(listdata)
+
+    })
+
 };
 
 //function UV
 
 function searchUV(lat, lon) {
-   // console.log(lat,lon)
-   
-    fetch(`http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`)
-        .then(function (response) {
-      
-            return response.json()
-        }).then(function (data) {
-           
-            //get the UV data
-           var ultra=data.value;
+  // console.log(lat,lon)
 
-           //create the button
-           var buttonUVEL=document.createElement("button");
-           buttonUVEL.classList= "btn";        
-           buttonUVEL.textContent= ultra;
-          
-           //condicional for the conditions are favorable (green), moderate(yellow) or severe(red)
-          if (ultra < 3) {
-            buttonUVEL.classList.add("btn-success");
-          }
-          else if (ultra < 7) {
-            buttonUVEL.classList.add("btn-warning");
-          }
-          else {
-            buttonUVEL.classList.add("btn-danger");
-          }
+  fetch(`http://api.openweathermap.org/data/2.5/uvi?appid=${apiKey}&lat=${lat}&lon=${lon}`)
+    .then(function (response) {
 
-          var UVel = document.createElement("div");
-          UVel.innerText = "UV Index:"+" ";
-          UVel.appendChild(buttonUVEL);
+      return response.json()
+    }).then(function (data) {
 
-          //display on page the UV data
-           listdata.appendChild(UVel)
-           weatherInfo.appendChild(listdata)
-        })
+      //get the UV data
+      var ultra = data.value;
+
+      //create the button
+      var buttonUVEL = document.createElement("button");
+      buttonUVEL.classList = "btn";
+      buttonUVEL.textContent = ultra;
+
+      //condicional for the conditions are favorable (green), moderate(yellow) or severe(red)
+      if (ultra < 3) {
+        buttonUVEL.classList.add("btn-success");
+      }
+      else if (ultra < 7) {
+        buttonUVEL.classList.add("btn-warning");
+      }
+      else {
+        buttonUVEL.classList.add("btn-danger");
+      }
+
+      var UVel = document.createElement("div");
+      UVel.innerText = "UV Index:" + " ";
+      UVel.appendChild(buttonUVEL);
+
+      //display on page the UV data
+      listdata.appendChild(UVel)
+      weatherInfo.appendChild(listdata)
+    })
 }
 
 
@@ -111,44 +119,81 @@ function searchUV(lat, lon) {
 
 function searchForecast(city) {
 
-       fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-          console.log("forescat",data)
+  fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log("forescat", data)
 
-        //queremos los indices de 4, 12, 20, 28, y 36
-        //nos da la informacion de noon por cada dia
-            console.log("displaying temps");
-      /*  for (var i = 4; i < 37; i += 8) {
-            console.log(data.list[i].main.humidity)
-            console.log(data.list[i].main.temp)
-        }*/
-        var titleforecast=document.createElement("h2")
-        var firstDT=document.createElement("p")
-        var firstDH=document.createElement("p")
-        var container=document.createElement("div")
+     
+      var titleforecast = document.createElement("h2")
+      
+      var firstDT = document.createElement("p")
+      var firstDH = document.createElement("p")
+      var secDT = document.createElement("p")
+      var sectDH = document.createElement("p")
+      var thiDT = document.createElement("p")
+      var thiDH = document.createElement("p")
+      var fourDT = document.createElement("p")
+      var fourtDH = document.createElement("p")
+      var fifDT = document.createElement("p")
+      var fifDH = document.createElement("p")
+     // var container = document.createElement("div")
 
-       // var secondDT=data.list[12].main.humidity;
-       // var secondDH=data.list[12].main.temp;
-        
-        titleforecast.textContent="5-Day Forecast:"
-        container.classList='card text-white bg-primary mb-3" style="max-width: 18rem;'
-        firstDT.textContent = " Temperature:" + " " + data.list[4].main.temp + " " + "ºF";
-        firstDH.textContent = " Humidity:" + " " +data.list[4].main.humidity;+ " " + "%";
-        
-        //show  5 day forecast
-        divAllDay.appendChild(firstDT)  
-        divAllDay.appendChild( firstDH) 
-        container.appendChild(divAllDay)
-           
-        daysFore.appendChild(titleforecast)
-        daysFore.appendChild( container)
+      
+      // tittle for show each day
+      titleforecast.textContent = "5-Day Forecast:"
+      
+      // container.classList='card text-white bg-primary mb-3" style="max-width: 18rem;'
+      firstDT.textContent = " Temperature:" + " " + data.list[4].main.temp + " " + "ºF";
+      firstDH.textContent = " Humidity:" + " " + data.list[4].main.humidity; + " " + "%";
+      day1.classList = 'text-white bg-primary  style  ';
 
+      //second day
+      secDT.textContent = " Temperature:" + " " + data.list[12].main.temp + " " + "ºF";
+      sectDH.textContent = " Humidity:" + " " + data.list[12].main.humidity; + " " + "%";
+      day2.classList = ' text-white bg-primary style';
 
+      //third day
+      thiDT.textContent = "Temperature:" + " " + data.list[20].main.temp + " " + "ºF";
+      thiDH.textContent = " Humidity:" + " " + data.list[20].main.humidity; + " " + "%";
+      day3.classList = 'text-white bg-primary  style';
 
-     })
+      //fourth day
+      fourDT.textContent = "Temperature:" + " " + data.list[28].main.temp + " " + "ºF";
+      fourtDH.textContent = " Humidity:" + " " + data.list[28].main.humidity; + " " + "%";
+      day4.classList = 'text-white bg-primary  style';
+
+      //fifth day
+      fifDT.textContent = "Temperature:" + " " + data.list[32].main.temp + " " + "ºF";
+      fifDH.textContent = " Humidity:" + " " + data.list[32].main.humidity; + " " + "%";
+      day5.classList = 'text-white bg-primary  style';
+
+      //show  5 day forecast
+       //clear the info
+      title.textContent="";
+      day1.textContent="";
+      day2.textContent="";
+      day3.textContent="";
+      day4.textContent="";
+      day5.textContent="";
+
+     //display the info
+      title.appendChild(titleforecast)
+      day1.appendChild(firstDT)
+      day1.appendChild(firstDH)
+      day2.appendChild(secDT)
+      day2.appendChild(sectDH)
+      day3.appendChild(thiDT)
+      day3.appendChild(thiDH)
+      day4.appendChild(fourDT)
+      day4.appendChild(fourtDH)
+      day5.appendChild(fifDT)
+      day5.appendChild(fifDH)
+      
+      
+    })
 }
 
 /*
@@ -164,18 +209,18 @@ function searchForecast(city) {
 // listener onclick button
 
 document.getElementById("searchC").addEventListener("click", function (event) {
-    event.preventDefault();
-    var cityIn = document.getElementById("city").value;
+  event.preventDefault();
+  var cityIn = document.getElementById("city").value;
 
-    if (cityIn) {
-        // verificar si existe en el array y si no existe entonce push el el array y salvar en local storage // show the list
-        searchCurrent(cityIn);
-        searchForecast(cityIn);
-        //limpiar el input
-    } else {
-        alert("You need in a City")
-    }
-  
+  if (cityIn) {
+    // verificar si existe en el array y si no existe entonce push el el array y salvar en local storage // show the list
+    searchCurrent(cityIn);
+    searchForecast(cityIn);
+    //limpiar el input
+  } else {
+    alert("You need in a City")
+  }
+
 })
 
 
